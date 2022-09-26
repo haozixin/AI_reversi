5e7d293d4cd87c8eb54d42f49b0d18c5415724f2
zyinghua <yzho0149@student.monash.edu>
2022-09-26 21:29:19 +1000

    Added mobility heuristics with dynamic weights that are subject to change

--- agents/t_004/myTeam.py
+++ agents/t_004/myTeam.py
@@ -6,6 +6,9 @@ from Reversi.reversi_model import ReversiGameRule
 from Reversi.reversi_utils import Cell, GRID_SIZE, countScore
 from template import Agent
 
+EARLY_GAME_THRESHOLD = 28
+LATE_GAME_THRESHOLD = 57
+
 
 class myAgent(Agent):
     def __init__(self, _id):
@@ -15,9 +18,10 @@ class myAgent(Agent):
 
     def SelectAction(self, actions, game_state):
         actions = list(set(actions))
-        return self.minimax(self.depth, self.agent_id, game_state, actions, -math.inf, math.inf)[1]
 
-    def minimax(self, depth, currentPlayer, game_state, actions, alpha, beta):
+        return self.minimax(self.depth, self.agent_id, game_state, actions, -math.inf, math.inf, 0, 0)[1]
+
+    def minimax(self, depth, currentPlayer, game_state, actions, alpha, beta, mobility, mobility_op):
         """
         A function which maximises the heuristic hence the win rate of
         the agent and minimises the opponent agent. This function assumes
@@ -32,7 +36,8 @@ class myAgent(Agent):
         """
 
         if depth == 0 or gameEnds(game_state):
-            return self.evaluation(game_state, [30, 25]), None
+            return self.evaluation(game_state, self.selectWeightSet(getTotalNumOfPieces(game_state)),
+                                   mobility, mobility_op), None
 
         if currentPlayer == self.agent_id:  # It's our turn, we want to maximise
             maxEval = (-math.inf, None)
@@ -41,7 +46,8 @@ class myAgent(Agent):
                 game_state = generateSuccessor(game_state, action, currentPlayer)
                 nextPlayer = getNextAgentIndex(currentPlayer)
                 eval = self.minimax(depth - 1, nextPlayer, game_state,
-                                    getLegalActions(game_state, nextPlayer), alpha, beta)[0]
+                                    getLegalActions(game_state, nextPlayer), alpha, beta,
+                                    mobility + (0 if mobility_op == 0 else len(actions)), mobility_op)[0]
                 maxEval = max(maxEval, (eval, action), key=lambda x: x[0])
                 alpha = max(alpha, eval)
                 if beta <= alpha:
@@ -55,7 +61,8 @@ class myAgent(Agent):
                 game_state = generateSuccessor(game_state, action, currentPlayer)
                 nextPlayer = getNextAgentIndex(currentPlayer)
                 eval = self.minimax(depth - 1, nextPlayer, game_state,
-                                    getLegalActions(game_state, nextPlayer), alpha, beta)[0]
+                                    getLegalActions(game_state, nextPlayer), alpha, beta,
+                                    mobility, mobility_op + len(actions))[0]
                 minEval = min(minEval, (eval, action), key=lambda x: x[0])
                 beta = min(beta, eval)
                 if beta <= alpha:
@@ -63,14 +70,22 @@ class myAgent(Agent):
 
             return minEval
 
-    def evaluation(self, game_state, weights):
+    def evaluation(self, game_state, weights, mobility, mobility_op):
         print("Evaluation: ")
         print("Corners:", self.cornerHeuristic(game_state))
+        print("Mobility", self.mobilityHeuristic(mobility, mobility_op))
+        print("Mobility us:", mobility)
+        print("Mobility op:", mobility_op)
         print("pieces:", self.pieceCountHeuristic(game_state))
-        return weights[0] * self.cornerHeuristic(game_state) + weights[1] * self.pieceCountHeuristic(game_state)
+        return weights[0] * self.cornerHeuristic(game_state) \
+               + weights[1] * self.pieceCountHeuristic(game_state) \
+               + weights[2] * self.mobilityHeuristic(mobility, mobility_op)
 
-    def mobilityHeuristic(self):
-        pass
+    def mobilityHeuristic(self, mobility, mobility_op):
+        if (mobility + mobility_op) != 0:
+            return 100 * (mobility - mobility_op) / (mobility + mobility_op)
+        else:
+            return 0
 
     def cornerHeuristic(self, game_state):
         cornersCaptured = 0
@@ -106,6 +121,43 @@ class myAgent(Agent):
         opScore = countScore(game_state.board, GRID_SIZE, game_state.agent_colors[getNextAgentIndex(self.agent_id)])
 
         return 100 * (score - opScore) / (score + opScore)  # denominator always != 0 because of the game initialisation
+
+    def selectWeightSet(self, curr_board_time):
+        """
+        Dynamically changing the weights based on the
+        current board situation (time), measured in
+        the number of pieces on the board.
+        """
+        weight_sets = [
+            [70, -20, 8],  # EARLY
+            [70, -5, 15],  # MIDDLE
+            [70, 20, 10],  # 57
+            [70, 35, 5],   # 58
+            [70, 50, 3],  # 59
+            [70, 60, 3],  # 60
+            [70, 65, 3],  # 61
+            [70, 70, 3],  # 62
+            [70, 100, 3],  # 63
+        ]
+
+        if curr_board_time < EARLY_GAME_THRESHOLD:
+            return weight_sets[0]
+        elif curr_board_time < LATE_GAME_THRESHOLD:
+            return weight_sets[1]
+        elif curr_board_time == LATE_GAME_THRESHOLD:
+            return weight_sets[2]
+        elif curr_board_time == LATE_GAME_THRESHOLD + 1:
+            return weight_sets[3]
+        elif curr_board_time == LATE_GAME_THRESHOLD + 2:
+            return weight_sets[4]
+        elif curr_board_time == LATE_GAME_THRESHOLD + 3:
+            return weight_sets[5]
+        elif curr_board_time == LATE_GAME_THRESHOLD + 4:
+            return weight_sets[6]
+        elif curr_board_time == LATE_GAME_THRESHOLD + 5:
+            return weight_sets[7]
+        elif curr_board_time == LATE_GAME_THRESHOLD + 6:
+            return weight_sets[8]
 
 
 def generateSuccessor(game_state, action, agent_id):
@@ -183,3 +235,19 @@ def validPos():
 
 def getNextAgentIndex(agent_id):
     return (agent_id + 1) % 2
+
+
+def getTotalNumOfPieces(game_state):
+    """
+    Returns the total number of
+    pieces on the board in the current
+    game state.
+    """
+    count = 0
+
+    for i in range(GRID_SIZE):
+        for j in range(GRID_SIZE):
+            if game_state.board[i][j] != Cell.EMPTY:
+                count += 1
+
+    return count
